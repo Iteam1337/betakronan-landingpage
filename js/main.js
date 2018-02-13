e49531c87dedc3eaa98878b7d27100ef06997487
Annie Thorell <annie.thorell@gmail.com>
2018-02-13 16:22:09 +0100

    Add new navigation and top bar

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Top section animation
+$(window).scroll(function() {
+  $('.top-section').addClass('moved')
+  if($('.top-section').offset().top === 0) {
+    $('.top-section').removeClass('moved')
+  }
+});
+
+//Navigation animations
+$('.navigation .icon').on('click', function() {
+  $('.icon').toggleClass('open')
+  $('.links').toggleClass('show')
+  $('.close-text').toggleClass('show')
+})
+
+$('.links-grid a').on('click', function() {
+  $('.icon').removeClass('open')
+  $('.links').removeClass('show')
+  $('.close-text').removeClass('show')
+})
